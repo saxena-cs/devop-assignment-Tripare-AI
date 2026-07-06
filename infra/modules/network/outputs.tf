1516f5619aad539fb109a91ea992a879f6335683
Vaibhav Saxena <69432969+saxena-cs@users.noreply.github.com>
2026-07-06 20:48:45 +0530

    Add outputs for network module in Terraform
    Define outputs for VPC, subnets, and security groups.

---
+++ infra/modules/network/outputs.tf
@@ -0,0 +1,28 @@
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_app_subnet_ids" {
+  value = aws_subnet.private_app[*].id
+}
+
+output "private_db_subnet_ids" {
+  value = aws_subnet.private_db[*].id
+}
+
+output "alb_sg_id" {
+  value = aws_security_group.alb.id
+}
+
+output "ecs_sg_id" {
+  value = aws_security_group.ecs.id
+}
+
+output "rds_sg_id" {
+  value = aws_security_group.rds.id
+}
